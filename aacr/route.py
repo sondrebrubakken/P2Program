d47a86f9475c86dc53d62876d97356b93e187ee5
oscargroenberg <43586555+oscargroenberg@users.noreply.github.com>
2022-04-16 21:43:24 +0200

    version 1

--- aacr/route.py
+++ aacr/route.py
@@ -1,6 +1,16 @@
+from turtle import title
 from flask import render_template
 from aacr import app
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/cal")
+def cal():
+    return render_template("cal.html", title="Kalender")
+
+@app.route("/routes")
+def routes():
+    return render_template("routes.html", title="Ruter")
