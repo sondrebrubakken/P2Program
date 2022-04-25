1f080750502fc3a9436a423cab102fe6d8978263
Sondre Brubakken <sondrebrubakken@gmail.com>
2022-04-25 14:16:06 +0200

    testinger123

---
+++ aacr/forms.py
@@ -0,0 +1,6 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegisterForm(FlaskForm):
+    
