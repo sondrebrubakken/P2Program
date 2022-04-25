7f528fa889598e4364d224800d83cc70a7a26ef5
Sondre Brubakken <sondrebrubakken@gmail.com>
2022-04-25 12:47:47 +0200

    testing1212

--- aacr/__init__.py
+++ aacr/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "1233333"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@localhost/P2'
+db = SQLAlchemy(app)
+
 
 from aacr import route
---
+++ aacr/model.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from aacr import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(55), nullable=False)
+
+
+class Rute(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rute = db.Column(db.String(30))
